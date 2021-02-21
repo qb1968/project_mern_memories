fdbedd8ea3c0b8c634d9b4b2eafa4d9db1aa7289
qb1968 <qb1968@aol.com>
2021-02-21 17:39:17 -0500

    Eslint correction

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -33,7 +33,7 @@ const Navbar = () => {
     }
 
     setUser(JSON.parse(localStorage.getItem('profile')));
-  }, [location]);
+  }, [location]); //eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">

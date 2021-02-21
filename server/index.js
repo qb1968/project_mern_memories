3df37fd5777016ac3616e9df97a23f00369c5772
qb1968 <qb1968@aol.com>
2021-02-20 22:21:35 -0500

    Minor change

--- server/index.js
+++ server/index.js
@@ -16,11 +16,15 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
+app.get('/', (req,res) => {
+  res.send('Hello to Memories API')
+})
+
 const CONNECTION_URL = 'mongodb+srv://qb1968:qball2@cluster0.c5lso.mongodb.net/memories?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
